e9e1f352118afd2d53c93058651b17a7f97d86da
JeffyLo94 <jeffrey.lo94@yahoo.com>
2019-10-22 11:13:05 -0700

    Update test-dht-sensor.py

--- test-dht-sensor.py
+++ test-dht-sensor.py
@@ -3,7 +3,11 @@ import sys
 import Adafruit_DHT
 
 while True:
+    sensor = Adafruit_DHT.DHT11
+    # GPIO Port
+    # Jeffrey - 
+    gpio = 7
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 
-    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+    print('Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temperature, humidity))
